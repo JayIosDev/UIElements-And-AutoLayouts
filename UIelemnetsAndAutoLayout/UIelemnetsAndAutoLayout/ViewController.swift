1ebc2c2a853ef3c20e14ebe70e000a2b4b08fccb
jayaram G <43853836+Jayaram143@users.noreply.github.com>
2018-12-13 13:32:21 +0530

    added some UI

---
+++ UIelemnetsAndAutoLayout/UIelemnetsAndAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIelemnetsAndAutoLayout
+//
+//  Created by Jayaram G on 13/12/18.
+//  Copyright © 2018 Jayaram G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    
+    
+    
+    
+    
+}
+
